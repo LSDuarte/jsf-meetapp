0e16eaa5b42df5696bb1394b014c9d2129a8cce4
Larissa Duarte <larissaduarte93@gmail.com>
2021-05-17 18:46:43 -0400

    iniciando tela de cadastro de eventos

---
+++ src/main/java/br/com/dio/controller/EventoBean.java
@@ -0,0 +1,8 @@
+package br.com.dio.controller;
+
+import java.io.Serializable;
+
+public class EventoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/br/com/dio/model/Evento.java
@@ -0,0 +1,9 @@
+package br.com.dio.model;
+
+import java.io.Serializable;
+
+public class Evento implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
